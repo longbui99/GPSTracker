58912abf3184efdea1bbeca014a6e12c820bc698
Bui Long <long.builong.bui@hcmut.edu.vn>
2020-06-16 09:34:13 +0700

    make it better

--- pj_module/Auth/AuthModules/localAuth.js
+++ pj_module/Auth/AuthModules/localAuth.js
@@ -1,6 +1,4 @@
 const LocalStrategy = require('passport-local').Strategy;
-const DBMS = require('../../config/DBMS.js')
-const Hash = require('../AuthSecure/Hash.js')
 const AuthFunction = require('../AuthFunction/AuthFunction.js')
 module.exports = (passport, User) => {
     passport.use('authClient',new LocalStrategy( AuthFunction.LocalStragegyClient

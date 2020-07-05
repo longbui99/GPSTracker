6924a6487538f81d61c1756a22d70feab42b6307
= <=>
2020-07-05 14:22:34 +0700

    update main device ui

--- pj_module/Client/DviceMain/DviceMainExport.js
+++ pj_module/Client/DviceMain/DviceMainExport.js
@@ -1,7 +1,25 @@
+const DBMS = require("../../Config/DBMS");
 
 // app: Server listening request and response to client side
 // User: Database
 
-module.exports = function(app, User, ObjectId){
-    // Your code here
-}
+module.exports = function (app, Users, ObjectId) {
+  Users.collection("GPSDeviceCollection")
+    .find({})
+    .toArray((err, res) => {
+      if (err) throw err;
+      //console.log("GPSDeviceCollection");
+      //console.log(res);
+    });
+
+  app.post("/cli-main/get-devices-client-information", (req, res) => {
+    Users.collection(DBMS.ClientDeviceControl)
+      .find({
+        OwnerId: req.user.id,
+        GPSID: req.body.GPSID,
+      })
+      .toArray(function (err, response) {
+        res.send(response);
+      });
+  });
+};
